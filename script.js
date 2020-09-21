e8b08e780d05f4e6ef7cf9006c80fb5e9f14ebbb
Adilson Dias <adilsondias@outlook.com>
2020-09-21 00:06:00 -0300

    Labirinto completo agora falta alguns ajutes de layout

--- script.js
+++ script.js
@@ -13,70 +13,186 @@ const map = [
     "W     W W W   W W W W",
     "W WWWWWWW WWWWW W W W",
     "W       W       W   W",
-    "WWWWWWWWWWWWWWWWWWWWW",
+    "WWWWWWWWWWWWWWWWWWWWW"
 ];
 
 
 
-// let resultado = map.map(function(e){
 
+for(let linha = 0; linha< map.length; linha++){
+
+ let linhaPalco = document.createElement('div')
+ linhaPalco.className = 'linha'
+ linhaPalco.id = `linha${linha + 1}`
+ document.getElementById('jogo').appendChild(linhaPalco)
+
+
+ for ( let coluna = 0; coluna < map[linha].length; coluna++){
+     
+    let celula = document.createElement('div')
+
+     if (map[linha][coluna] === 'W'){
+        
+        celula.className = 'parede'
     
-//     if( e === 'w'){
+     }else if( map[linha][coluna] === ' ' ||  map[linha][coluna] === 'S' ||  map[linha][coluna] === 'F'){
+         celula.className = 'piso'
+     }
+     celula.classList.add(`celula${coluna + 1}`)
 
-//      e = 'tijolo'
+     if( map[linha][coluna] === 'S'){
+         celula.id = 'start'
+         let img = document.createElement('img')
+         img.id = 'jogador'         
+         img.src = './img/jogador1.png'
+         celula.appendChild(img)
 
-//      return console.log(e);
+     }
 
-//     }
-// })
+     if(map[linha][coluna] === 'F'){  
+         
+        celula.id = 'finish'
+        let img = document.createElement('img')
+        img.id = 'jogaorVence'
+        img.src = './img/vencedor.png'
+     
+     }
+     document.getElementById(`linha${linha + 1}`).appendChild(celula)
 
+ }
+}
 
-for( let i = 0 ; map.length; i++){
+let jogador = document.getElementById('jogador')
+// pega a celula na qual o jogador e filho
+let celula = jogador.parentElement 
+// pega linha na qual a celula é filha.
+let linha = celula.parentElement 
 
-    console.log('linha' + map[i]);
-    for( let j = 0; map.length; j++){
+document.addEventListener('keydown', (e) =>{
 
-        console.log('coluna'[0][0])
+// recebendo o que foi digitado pelo jogador
+const teclapressionada = e.key
+
+if(teclapressionada ==='ArrowUp'){
+    if(linha.previousSibling !== null){
+        linha = linha.previousSibling
+    }
+    let posicaoAtual = celula.classList[1]
+    let posicaoFilha = linha.childNodes
+
+     
+    // console.log(linha);
+    // console.log(posicaoAtual);
+    // console.log(posicaoFilha);
+
+    for( let i = 0; i< posicaoFilha.length; i++){
+       
+        // let valorAtual = posicaoFilha[i].classList.contains(posicaoAtual) 
+        // let piso = posicaoFilha[i].classList.contains('piso')
+
+        if(posicaoFilha[i].classList.contains(posicaoAtual)){
+        if( posicaoFilha[i].classList.contains('piso')){
+            
+                celula = posicaoFilha[i]
+            }else{
+               linha = linha.nextSibling            
+        }
+        }
+
+        // console.log("Filha" + posicaoFilha[i].classList.contains(posicaoAtual));
+        // console.log("Atual" + posicaoAtual);
     }
 
-    console.log(map.length)
+    celula.appendChild(jogador)
+}else if( teclapressionada === 'ArrowLeft'){
+
+    if(celula.previousSibling === null){
+        
+        console.log('movimento invalido')
+    }else if(celula.previousSibling.classList.contains('piso')){
+        celula = celula.previousSibling
+        celula.appendChild(jogador)
+    }
+}else if( teclapressionada === 'ArrowRight'){
+    
+    if(celula.nextSibling === null ){
+    console.log('movimento invalido');
+   }else if(celula.nextSibling.classList.contains('piso')){
+       
+    celula = celula.nextSibling
+    celula.appendChild(jogador)
+   }
+}else if(teclapressionada === 'ArrowDown'){
+    if( linha.nextSibling !== null){
+        linha = linha.nextSibling
+    }
+
+    let posicaoAtual = celula.classList[1]
+    let posicaoFilha = linha.childNodes
+    for(let i = 0; i< posicaoFilha.length; i++){
+        if(posicaoFilha[i].classList.contains(posicaoAtual)){
+        if(posicaoFilha[i].classList.contains('piso')){
+
+            celula = posicaoFilha[i]
+        }else{
+
+            linha = linha.previousSibling
+        }
+        }
+
+
+    }
+    celula.appendChild(jogador)
+}else{
+
+    let posicaoAtual = celula.classList[1]
+    let posicaoFilha = linha.childNodes
+    for(let i = 0; i< posicaoFilha.length; i++){
+        if(posicaoFilha[i].classList.contains(posicaoAtual)){
+        if(posicaoFilha[i].classList.contains('finish')){
+
+            celula = posicaoFilha[i]
+        }else{
+
+            linha = linha.previousSibling
+        }
+        }
+
+
+    }
+    celula.appendChild(jogador) 
+}
+
+verificaVencedor()
+
+})
+
+let p = document.createElement('p')
+
+function verificaVencedor(){
+
+    if(jogador.parentElement.id === 'finish'){
+              
+        document.getElementById('mensagem').classList.remove('hide')         
+
+    }
+    reiniciar = document.querySelector('button')
+
+    reiniciar.addEventListener('click', reiniciarJogo)
+
+}
+
+function reiniciarJogo(){
+    
+    window.location.reload()
 }
 
 
-let resultado = map.map(function(valor, inde e){
-
-    return console()
-  
-})
 
 
 
-
-
-// for (let i = 0; i < map.length; i++) {
-//     let outerCell = document.createElement('div')
-//     outerCell.className = 'line'
-//     outerCell.id = `line${i+1}`
-//     document.getElementById('game').appendChild(outerCell)
-
-//     for (let j = 0; j < map[i].length; j ++) {
-//         let cell = document.createElement('div')
-//         if (map[i][j] === 'W') {
-//             cell.className = 'wall'
-//         } else if(map[i][j] === ' ' || map[i][j] === 'S' || map[i][j] === 'F') {
-//             cell.className = 'floor'
-//         }
-//         cell.classList.add(`column${j+1}`)
-        
-//         if (map[i][j] === 'S') {
-//             cell.id = 'start'
-//             let img = document.createElement('img')
-//             img.id = 'player'
-//             img.src = './src/peao.png'
-//             cell.appendChild(img)
-//         } else if (map[i][j] === 'F') {
-//             cell.id = 'finish'
-//         }
-//         document.getElementById(`line${i+1}`).appendChild(cell)
-//     }
-// }
+// pergutnas 
+/**
+ * Qual a diferenca entre elementSibiling e Sibiling
+ * 
+ */
--- teste1.js
+++
@@ -1,3 +0,0 @@
-jogo = document.getElementById('jogo')
-
-
